2c2c7b387e656f5dc7049d06352f2e2fc0c93544
Mohd Zaid <mdzaid1299@gmail.com>
2025-02-27 13:04:46 +0530

    retry

--- hindu-news-app/app.py
+++ hindu-news-app/app.py
@@ -22,6 +22,5 @@ def home():
     return render_template('index.html', news=news)
 
 if __name__ == "__main__":
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
